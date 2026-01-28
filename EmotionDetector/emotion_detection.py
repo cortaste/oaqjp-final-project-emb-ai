f3f1c827a9f8c82ba77a1ec1b5d09c8a4b606617
Stefan Cortali <stefan.cortali@gmail.com>
2026-01-28 11:09:50 -0500

    add stuff

--- EmotionDetector/emotion_detection.py
+++ EmotionDetector/emotion_detection.py
@@ -1,8 +1,17 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input = { "raw_document": { "text": text_to_analyze } }
-    response =requests.post(url, json=input, headers=header)
-    return response.text
+    response = json.loads(requests.post(url, json=input, headers=header).text)['emotionPredictions'][0]['emotion']
+    m_emotion = max(response, key=response.get)
+    return {
+        'anger': response['anger'],
+        'disgust': response['disgust'],
+        'fear': response['fear'],
+        'joy': response['joy'],
+        'sadness': response['sadness'],
+        'dominant_emotion': m_emotion
+        }
+
